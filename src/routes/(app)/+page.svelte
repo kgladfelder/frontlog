b51913610c2e4db1eb98f5c3237421e3e3cfa722
Kyle <39464447+kgladfelder@users.noreply.github.com>
2026-02-05 22:25:35 -0600

    chore: change notes

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -10,5 +10,7 @@
 
 <div class="text-sm/6 font-ibm-plextext-foreground dark:text-foreground-dark">
 	<h1>Hi, {data.user.username}!</h1>
-	<p>You probably want to read up on doing this and getting it saved in user preferences <a class="text-blue-500 dark:text-sky-500" target="_blank" href="https://tailwindcss.com/docs/dark-mode#with-system-theme-support">Dark Mode With System Theme Support</a></p>
+	<p>Time to start doing data structure stuff.</p>
+	<p>Metadata structure defined in user settings and then stored per entity in the same format.</p>
+	<p>Figure out how to do metadata changes after a tracking section is defined.</p>
 </div>
